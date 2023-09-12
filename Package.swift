b55d711120f7d06b2f96d2768510108826a23527
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-09-12 10:04:47 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MiamPublicSPM",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MiamPublicSPM",
+            targets: ["miamCore"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "miamCore",
+            path: "Sources/miamCore.xcframework"
+        )
+    ]
+)
